9e732af70aac054bc777495807dfc33b0c1cdea7
ekirimlioglu <ekirimli@ucsd.edu>
2023-06-04 17:28:28 -0700

    update sql

--- Application/Website/Customers/initDB.sql
+++ Application/Website/Customers/initDB.sql
@@ -4,16 +4,34 @@ use aegis;
 -- DUMP EVERYTHING... YOU REALLY SHOULDN'T DO THIS!
 drop table if exists customers;
 drop table if exists sessions;
+drop table if exists datas;
+
 
 CREATE TABLE customers (
     ID INT PRIMARY KEY AUTO_INCREMENT,
-    dongleID VARCHAR(255) NOT NULL UNIQUE,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR (255) NOT NULL,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE devices (
+    ID int primary key AUTO_INCREMENT,
+    name varchar(255) not null,
+    customerID int,
+    FOREIGN KEY (customerID) REFERENCES customers(ID)
+)
+
+create table datas (
+    ID int primary key AUTO_INCREMENT,
+    timestamp timestamp not null default current_timestamp,
+    temp float,
+    hum float,
+    current float,
+    senderID int,
+    FOREIGN KEY (senderID) REFERENCES devices(ID)
+)
 
 create table if not exists sessions (
   session_id varchar(64) primary key,
