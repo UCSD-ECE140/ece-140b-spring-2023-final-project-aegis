63d41491c23e162dbd3e0440c5aee585b5359ad4
Kevin Shin <kevshin2002@gmail.com>
2023-06-04 21:47:54 -0700

    Credentials for MQTT

--- Application/Website/utilities/MQTTServer.py
+++ Application/Website/utilities/MQTTServer.py
@@ -1,14 +1,26 @@
+import os                                         # Used for interacting with the system environment
+from dotenv import load_dotenv                    # Used to read the credentials
 import paho.mqtt.client as mqtt
+load_dotenv('credentials.env')                 # Read in the environment variables for MySQL
+
+MQTT_config = {
+  "server": os.environ['MQTT_SERVER'],
+  "username": os.environ['MQTT_USERNAME'],
+  "password": os.environ['MQTT_PASSWORD'],
+  "domain": os.environ['MQTT_DOMAIN'],
+  "port": os.environ['MQTT_PORT'],
+  "topic": os.environ['MQTT_TOPIC']
+}
 
 class MQTTServer:
     def __init__(self):
-        self.client = mqtt.Client(client_id='AegisServer', clean_session=True)
-        self.client.username_pw_set(username="aegisAdmin", password='iLoveAegis!')
+        self.client = mqtt.Client(client_id = MQTT_config['server'], clean_session=True)
+        self.client.username_pw_set(username = MQTT_config['username'], password = MQTT_config['password'])
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
 
     def start(self):
-        self.client.connect_async("aegishome.ninja", 8003)
+        self.client.connect_async(MQTT_config['domain'], int(MQTT_config['port']))
         self.client.loop_start()
 
     def stop(self):
@@ -16,11 +28,11 @@ class MQTTServer:
 
     def on_connect(self, client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
-        client.subscribe("Aegis/#")
+        client.subscribe(MQTT_config['topic'] + "/#")
 
     def on_message(self, client, userdata, msg):
         split = msg.topic.split("/")
-        if split[0] == "Aegis":
+        if split[0] == "MQTT_config['topic']":
             print(split[1] + " " + msg.payload.decode())
 
 
