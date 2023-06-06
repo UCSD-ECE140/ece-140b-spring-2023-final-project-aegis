8cdda6f033585dcb24d979554d03dbb611dea33f
Kevin Shin <kevshin2002@gmail.com>
2023-06-06 14:57:05 -0700

    Update MQTTServer.py

--- Application/Website/utilities/MQTTServer.py
+++ Application/Website/utilities/MQTTServer.py
@@ -1,6 +1,7 @@
 import os                                         # Used for interacting with the system environment
 from dotenv import load_dotenv                    # Used to read the credentials
 import paho.mqtt.client as mqtt
+os.chdir("..")
 load_dotenv('credentials.env')                 # Read in the environment variables for MySQL
 
 datas_config = {
@@ -27,8 +28,8 @@ class MQTTServer:
         self.client.on_message = self.on_message
 
     def start(self):
-        self.client.connect_async(MQTT_config['domain'], int(MQTT_config['port']))
-        self.client.loop_start()
+        self.client.connect(MQTT_config['domain'], int(MQTT_config['port']))
+        self.client.loop_forever()
 
     def stop(self):
         self.client.loop_stop()
@@ -39,10 +40,13 @@ class MQTTServer:
 
     def on_message(self, client, userdata, msg):
         split = msg.topic.split("/")
-        if split[0] == "MQTT_config['topic']":
-            print(split[1] + " " + msg.payload.decode())
+        if split[0] == "Aegis":
+            if split[1] == "aegisDongleSend":
+                print(split[1] + " " + msg.payload.decode())
+           
 
-
-
-
-mqtt_server = MQTTServer()
+# If running the server directly from Python as a module
+if __name__ == "__main__":
+    print(MQTT_config)
+    mqtt_server = MQTTServer()  
+    mqtt_server.start()
