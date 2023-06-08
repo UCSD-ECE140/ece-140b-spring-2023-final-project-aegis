1dacb7cfd521fcc06f938992256e387a5e90e67e
dom500d <dorlando@ucsd.edu>
2023-06-07 18:38:54 -0700

    Fixed the set temperature aspet of the IOS app

--- Application/Mobile/IOS/AEGIS/AEGIS/ContentView.swift
+++ Application/Mobile/IOS/AEGIS/AEGIS/ContentView.swift
@@ -137,6 +137,7 @@ class MQTTDelegate: NSObject, ObservableObject, CocoaMQTTDelegate {
     func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
         message = "Connected"
         mqtt.subscribe("Aegis/aegisDongleSend/#")
+        mqtt.subscribe("Aegis/aegisTempSet")
     }
     
     func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
@@ -152,6 +153,10 @@ class MQTTDelegate: NSObject, ObservableObject, CocoaMQTTDelegate {
             return
         }
         let topic = message.topic.components(separatedBy: "/").last
+        if topic == "aegisTempSet" {
+            mqttDelegate.activeSetTemp = message.string
+            return
+        } 
         let data = payload.split(separator: ";")
         let deviceId = topic
         if let lastData = data.last {
